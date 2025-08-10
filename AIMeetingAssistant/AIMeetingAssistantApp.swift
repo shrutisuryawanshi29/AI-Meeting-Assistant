0835723742a4a35f82d1637c27055306e86b3cd6
shrutisuryawanshi <shrutisuryawanshi720@gmail.com>
2025-08-09 23:47:08 -0400

    Initial commit

---
+++ AIMeetingAssistant/AIMeetingAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIMeetingAssistantApp.swift
+//  AIMeetingAssistant
+//
+//  Created by Shruti Suryawanshi on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct AIMeetingAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIMeetingAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIMeetingAssistant
+//
+//  Created by Shruti Suryawanshi on 8/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
